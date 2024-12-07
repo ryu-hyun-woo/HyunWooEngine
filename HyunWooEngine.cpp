75fb229731c6ddef4fb029561670bb6de48cc7d3
류현우 <dalongyigame@naver.com>
2024-12-07 11:58:54 +0900

    INIT:
    TEST

--- HyunWooEngine.cpp
+++ HyunWooEngine.cpp
@@ -27,6 +27,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // TODO: 여기에 코드를 입력합니다.
 
+    //TEST
+
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_HYUNWOOENGINE, szWindowClass, MAX_LOADSTRING);
